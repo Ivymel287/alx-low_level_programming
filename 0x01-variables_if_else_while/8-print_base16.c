0c8961d4c8339c49ea9335ef9308fa8ab634aa16
Ivymel287 <ivymel287@gmail.com>
2022-03-14 13:40:46 +0300

    my ninth commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,20 @@
 
 int main(void)
 {
-	char ch = 'z';
-	int i = 26;
+	char ch = 'a';
+	int i = 48;
 	/* your code goes there */
-	while (i > 0)
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 0;
+	while (i < 6)
 	{
 		putchar(ch);
-		ch--;
-		i--;
+		ch++;
+		i++;
 	}
 	putchar('\n');
 	return (0);
