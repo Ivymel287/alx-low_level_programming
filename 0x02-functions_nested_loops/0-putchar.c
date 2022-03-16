4f67b16cb94f20d692e749fa56158ca995aee8fc
Ivymel287 <ivymel287@gmail.com>
2022-03-16 04:38:34 +0300

    my 0-putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Print the word "_putchar"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	char p[] = "_putchar\n";
+
+	while (p[i])
+		{
+		_putchar(p[i]);
+		i++;
+		}
+	return (0);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C0 (success)[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
