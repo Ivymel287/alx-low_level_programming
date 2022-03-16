1ddcf8e8b3dde93faffdaef3b9c1a2e0765d4340
Ivymel287 <ivymel287@gmail.com>
2022-03-16 03:15:15 +0300

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
 * main - Print the word "_putchar"
 *
 * Return: Always 0 (Success)
-*/
+*/[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Entry point[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
 
 int main(void)
 {
@@ -12,9 +12,9 @@ int main(void)
 	char p[] = "_putchar\n";
 
 	while (p[i])
-		{
+{
 		_putchar(p[i]);
 		i++;
 		}
 	return (0);
-}
+}[A[A[A[A[A[D                 {[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
