a0f4a8f98967f984ebb0a3db82fc35e299bf49ec
Ivymel287 <ivymel287@gmail.com>
2022-03-22 05:45:14 +0300

    my o task commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *reset_to_98 - update a integer value to 98[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Ds a integer value to 98
- *on: A pointer to an int that will be updated[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[@[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
  *
- *Return: void that mean is correct
+ *Return: void that means is correct
  */
 
 void reset_to_98(int *n)
