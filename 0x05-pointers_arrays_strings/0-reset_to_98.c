a122de0f26e6a6c87559b058af78da99f615e252
Ivymel287 <ivymel287@gmail.com>
2022-03-22 05:20:39 +0300

    my first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - update a integer value to 98[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Ds a integer value to 98
+ *on: A pointer to an int that will be updated[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[@[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D
+ *
+ *Return: void that mean is correct
+ */
+
+void reset_to_98(int *n)
+
+[
+*n = 98;
+]
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strien(char *5);
+void _put(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+[A[A[A[A[A[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[s[B[D[D[D[D[D[D[D[D[D[Ds(char *str);[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+int _atoi(char *s);
